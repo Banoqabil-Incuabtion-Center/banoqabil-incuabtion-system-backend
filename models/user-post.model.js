afdcfb6ca9150556df366d6384beb58b2736b554
muhammadshahms <muhammadshah516@gmail.com>
2025-12-16 03:05:43 +0500

    feat: add UserPost Mongoose model with schema and indexes

--- models/user-post.model.js
+++ models/user-post.model.js
@@ -11,4 +11,8 @@ const userPostSchema = new mongoose.Schema({
   deletedAt: { type: Date, default: null }
 })
 
+// Compound index for efficient querying of non-deleted posts sorted by newest
+userPostSchema.index({ deletedAt: 1, createdAt: -1 });
+userPostSchema.index({ user: 1, deletedAt: 1 });
+
 module.exports = mongoose.model('userpost', userPostSchema);
